94df379883e4729d3e15a8941cd3ffb124cc95a6
Arturo Salinas <prico.team@gmail.com>
2022-12-30 09:50:39 -0600

    removing unnecessary project - Creating new web ai project

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] model = { "Product1", "Product2", "Product3", "Product4" };
+            return View();
+        }
+    }
+}
